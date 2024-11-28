1d451cd791617ba433155a25a7d5fb2be3ee5ec9
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-28 12:08:49 +0700

    Update btvn10Sess7.cpp

--- btvn10Sess7.cpp
+++ btvn10Sess7.cpp
@@ -1,33 +1,28 @@
 #include<stdio.h>
 int main(){
-	int num[1];
 	int n;
 	int i;
-	int soNT=2;
 	int j;
-	int number;
 	int count=0;
-	
 	printf("nhap vao so phan tu cua mang: ");
-	scanf("%d",&num[n]);
-	
-	for(i=0;i<num[n];i++){
+	scanf("%d",&n);
+	int num[n];
+	for(i=0;i<n;i++){
 		printf("moi ban nhap vao so thu %d: ",i+1);
 		scanf("%d",&num[i]);
 	}
-	
-	while(number<num[n]){
+	i=0; 
+	while(i<n){
 		count=0;
-		for(int j=1; j<=soNT; j++){
-			if(soNT%j==0){
+		for(j=1; j<=num[i]; j++){
+			if(num[i]%j==0){
 				count++;
 			}
 		}
 		if(count==2){
-			printf("\n so ngguyen to: %d",soNT);
-			number++;
+			printf("\n so ngguyen to: %d",num[i]);
 		}
-		soNT++;
+		i++;
 	}
 	return 0;
 }
