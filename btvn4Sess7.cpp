eca95c457c5693552563f85081fad18d79bbf3ed
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-28 13:15:18 +0700

    Update btvn4Sess7.cpp

--- btvn4Sess7.cpp
+++ btvn4Sess7.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-	int num[0];
 	int n;
 	printf("nhap vao so phan tu cua mang: ");
 	scanf("%d",&n);
+	int num[n];
 	for(int i=0;i<n+1;i++){
 		printf("moi ban nhap vao so thu %d: ",1+i);
 		scanf("%d",&num[i]);
